853f20df750c5456e73b72fa137ccb3dd8fc7add
Pedro Oliveira <pedrogamer212004@gmail.com>
2024-08-17 13:05:49 -0300

    style
    Imagens sem fundo

---
+++ fundo.py
@@ -0,0 +1,25 @@
+import os
+from rembg import remove
+from PIL import Image
+
+# Define o caminho da pasta onde as imagens estão
+pasta = 'imgs'
+
+# Percorre todos os arquivos na pasta
+for nome_arquivo in os.listdir(pasta):
+    # Verifica se o arquivo é uma imagem com extensão .jpg, .png, etc.
+    if nome_arquivo.endswith(('.jpg', '.JPG')):
+        # Exibe o nome do arquivo
+        print(nome_arquivo)
+
+        # Constrói o caminho completo para o arquivo de entrada
+        img_entrada = os.path.join(pasta, nome_arquivo)
+
+        # Define o caminho completo para o arquivo de saída
+        img_saida = os.path.join(pasta, 'SFundo', f'{os.path.splitext(nome_arquivo)[0]}.PNG')
+
+        # Abre a imagem e processa com o rembg
+        input = Image.open(img_entrada)
+        output = remove(input)
+        output.save(img_saida)
+
