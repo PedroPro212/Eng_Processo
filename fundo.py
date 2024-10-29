46e461232e95f520bcea7fa1a5b8517a8a89e157
Oliveira <Pedro.Oliveira1@aptiv.com>
2024-10-29 08:45:37 -0300

    add: foto

--- fundo.py
+++ fundo.py
@@ -3,7 +3,7 @@ from rembg import remove
 from PIL import Image
 
 # Define o caminho da pasta onde as imagens estão
-pasta = 'imgFundo'
+pasta = 'imgsTeste'
 
 # Percorre todos os arquivos na pasta
 for nome_arquivo in os.listdir(pasta):
@@ -16,7 +16,7 @@ for nome_arquivo in os.listdir(pasta):
         img_entrada = os.path.join(pasta, nome_arquivo)
 
         # Define o caminho completo para o arquivo de saída
-        img_saida = os.path.join(pasta, 'sFundo', f'{os.path.splitext(nome_arquivo)[0]}.PNG')
+        img_saida = os.path.join(pasta, 'sem', f'{os.path.splitext(nome_arquivo)[0]}.PNG')
 
         # Abre a imagem e processa com o rembg
         input = Image.open(img_entrada)
